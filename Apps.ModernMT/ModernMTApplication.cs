5dca41969f5db844f7fad0001abec9f53685a2a9
vitalii-bezuhlyi <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2024-04-16 18:49:06 +0300

    Categories (#7)
    * added categories
    
    * Updated csproj
    
    ---------
    
    Co-authored-by: bZverok <137277669+bZverok@users.noreply.github.com>

--- Apps.ModernMT/ModernMTApplication.cs
+++ Apps.ModernMT/ModernMTApplication.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.ModernMT;
 
-public class ModernMtApplication : IApplication
+public class ModernMtApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.MachineTranslationAndMtqe];
+        set { }
+    }
+    
     public string Name
     {
         get => "Modern MT";
