d0210649892fe672a70b940a01190bd8aa86cd4b
Cecilia Nistal <cecilia.nistal@centific.com>
2024-05-06 07:21:23 -0300

    Added Estimate XLIFF action

---
+++ Apps.ModernMT/DataSourceHandlers/ConditionDataHandler.cs
@@ -0,0 +1,26 @@
+﻿using Blackbird.Applications.Sdk.Common.Dictionaries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.ModernMT.DataSourceHandlers
+{
+    public class ConditionDataHandler : IStaticDataSourceHandler
+    {
+        protected Dictionary<string, string> EnumValues => new()
+        {
+            { ">", "Score is above threshold" },
+            { ">=", "Score is above or equal threshold" },
+            { "=", "Score is same as threshold" },
+            { "<", "Score is below threshold" },
+            { "<=", "Score is below or equal threshold" }
+        };
+
+        public Dictionary<string, string> GetData()
+        {
+            return EnumValues;
+        }
+    }
+}
---
+++ Apps.ModernMT/Models/Quality/EstimateXliffResponse.cs
@@ -0,0 +1,14 @@
+﻿
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Files;
+
+namespace Apps.ModernMT.Models.Quality
+{
+    public class EstimateXliffResponse
+    {
+        public FileReference File { get; set; }
+
+        [Display("Average Score")]
+        public double AverageScore { get; set; }
+    }
+}
